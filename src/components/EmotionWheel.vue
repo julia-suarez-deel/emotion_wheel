929700ecb960e4b2f3878fe00a5b8cc209fbbaa1
Julia Suarez <js@plusteam.tech>
2020-12-30 18:35:49 -0400

    feat ⛄️: Setup echarts

--- src/components/EmotionWheel.vue
+++ src/components/EmotionWheel.vue
@@ -1,14 +1,71 @@
 <template>
-  <div>Hello!</div>
+  <base-chart :options="options" />
 </template>
 <script>
 import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import BaseChart from "./charts/BaseChart";
+import emotions from "@/assets/emotions.json";
+import "echarts/lib/chart/sunburst";
+import "echarts/lib/component/tooltip";
+import "zrender/lib/svg/svg";
 
-export default Vue.extend({
-  name: "App",
-
-  components: {},
-
-  data: () => ({})
-});
+@Component({
+  components: { BaseChart }
+})
+export default class EmotionWheel extends Vue {
+  options = {
+    title: {
+      text: "WORLD COFFEE RESEARCH SENSORY LEXICON",
+      subtext: "Source: https://worldcoffeeresearch.org/work/sensory-lexicon/",
+      textStyle: {
+        fontSize: 14,
+        align: "center"
+      },
+      subtextStyle: {
+        align: "center"
+      },
+      sublink: "https://worldcoffeeresearch.org/work/sensory-lexicon/"
+    },
+    series: {
+      type: "sunburst",
+      highlightPolicy: "ancestor",
+      data: emotions,
+      radius: [0, "95%"],
+      sort: null,
+      levels: [
+        {},
+        {
+          r0: "15%",
+          r: "35%",
+          itemStyle: {
+            borderWidth: 2
+          },
+          label: {
+            rotate: "tangential"
+          }
+        },
+        {
+          r0: "35%",
+          r: "70%",
+          label: {
+            align: "right"
+          }
+        },
+        {
+          r0: "70%",
+          r: "72%",
+          label: {
+            position: "outside",
+            padding: 3,
+            silent: false
+          },
+          itemStyle: {
+            borderWidth: 3
+          }
+        }
+      ]
+    }
+  };
+}
 </script>
---
+++ src/components/charts/BaseChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <e-chart ref="chart" v-bind="$attrs" :theme="theme" v-on="$listeners" />
+</template>
+<script>
+import EChart from "vue-echarts";
+import dark from "@/assets/echart-theme/dark";
+import light from "@/assets/echart-theme/light";
+import "echarts/lib/chart/bar";
+import "echarts/lib/component/tooltip";
+import "zrender/lib/svg/svg";
+import { Component } from "vue-property-decorator";
+
+EChart.registerTheme("dark", dark);
+EChart.registerTheme("light", light);
+@Component({
+  components: {
+    EChart
+  },
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    }
+  }
+})
+export default class BaseChart {}
+</script>
